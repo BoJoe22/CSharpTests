5491c863e570b17212156c168305e82b075f63e7
bojoe22 <josef.a.koza@gmail.com>
2014-01-13 23:49:25 -0500

    Add test for collections (Array) and reference objects in the collection.
    Quick example of potential side effects on reference objects and mutability.

---
+++ MethodsAndEncapsulationTests/ArrayReferenceTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+
+namespace MethodsAndEncapsulationTests
+{
+    [TestClass]
+    public class ArrayReferenceTests
+    {
+        /// <summary>
+        /// Be aware of object references in collections.  Potential side effects...
+        /// </summary>
+        [TestMethod]
+        public void ReferenceObjectsInArrays()
+        {
+            StringBuilder one = new StringBuilder("one");
+            StringBuilder two = new StringBuilder("two");
+
+            StringBuilder[] arr1 = { one, two };
+            StringBuilder[] arr2 = new StringBuilder[2];
+            arr1.CopyTo(arr2, 0);
+
+            one.Replace("one", "not one");
+
+            Assert.AreEqual("not one", arr1[0].ToString());
+            Assert.AreEqual("not one", arr2[0].ToString());
+        }
+    }
+}
