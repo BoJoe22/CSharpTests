d011bd6d33a6109f5beef0e13f329164a14f9641
unknown <JosefKoza@jkoza-00694.simventions.com>
2014-02-18 16:31:22 -0500

    Commit solution and example for using Code Contracts in Visual Studio

---
+++ CodeContracts/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+
+namespace CodeContracts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = null;
+            Greet(input);
+        }
+
+        public static int Greet(string[] args)
+        {
+            Contract.Requires(args != null);
+            Contract.Requires(args.Length != 0);
+            Contract.Requires(args[0] != null);
+
+            return args.Length;
+        }
+    }
+}
