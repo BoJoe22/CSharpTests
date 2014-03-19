1ab924f20778e10c350eef422e31b64774123057
bojoe22 <josef.a.koza@gmail.com>
2014-03-19 06:57:22 -0400

    Test Functional style in C Sharp
    Add new project for FunctionalCSharp.
    Play with Lambda expressions and Higher-Order functions.
    Create my own version of a Map extension method.

---
+++ FunctionalCSharpTests/FunctionalUtilitiesTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FunctionalCSharp;
+using System.Collections.Generic;
+
+namespace FunctionalCSharpTests
+{
+    [TestClass]
+    public class FunctionalUtilitiesTests
+    {
+        List<int> myList;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            myList = new List<int>();
+            for (int i = 1; i <= 10; i++)
+                myList.Add(i);
+        }
+
+        [TestMethod]
+        public void TestMap1()
+        {
+            List<int> newList = new List<int>(FunctionalUtilities.Map(myList, x => x * 2));
+            //var newList = FunctionalUtilities.Map(myList, x => x * 2);
+            //List<int> anotherList = new List<int>();
+            //foreach (var item in newList)
+            //    anotherList.Add(item);
+
+            Assert.AreEqual(10, myList.Count);
+            Assert.AreEqual(2, newList[0]);
+            Assert.AreEqual(4, newList[1]);
+            Assert.AreEqual(6, newList[2]);
+            Assert.AreEqual(8, newList[3]);
+            Assert.AreEqual(10, newList[4]);
+            Assert.AreEqual(20, newList[9]);
+            //Assert.AreEqual(new List<int>() { 2, 4, 6, 8, 10 }, newList);
+        }
+    }
+}
---
+++ FunctionalCSharpTests/HigherOrderFunTests.cs
@@ -0,0 +1,37 @@
+﻿using FunctionalCSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FunctionalCSharpTests
+{
+    [TestClass]
+    public class HigherOrderFunTests
+    {
+        List<int> myList;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            myList = new List<int>();
+            for (int i = 1; i <= 10; i++)
+                myList.Add(i);
+        }
+
+        [TestMethod]
+        public void TestSum1()
+        {
+            var result = HigherOrderFun.sum(myList, x => x);
+
+            Assert.AreEqual(55, result);
+        }
+
+        [TestMethod]
+        public void TestSum2()
+        {
+            var result = HigherOrderFun.sum(myList, x => x * 2);
+
+            Assert.AreEqual(110, result);
+        }
+    }
+}
