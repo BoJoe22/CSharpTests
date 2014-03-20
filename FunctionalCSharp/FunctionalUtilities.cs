ecfbdac057c33cf69bd0255d7595b2eedf08bad5
bojoe22 <josef.a.koza@gmail.com>
2014-03-19 20:26:59 -0400

    Commit missed files for Functional tests

---
+++ FunctionalCSharp/FunctionalUtilities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FunctionalCSharp
+{
+    public static class FunctionalUtilities
+    {
+        public static IEnumerable<U> Map<T, U>(this IEnumerable<T> myEnumerable, Func<T, U> f)
+        {
+            foreach (var item in myEnumerable)
+                yield return f(item);
+        }
+    }
+}
---
+++ FunctionalCSharp/HigherOrderFun.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FunctionalCSharp
+{
+    public class HigherOrderFun
+    {
+        public static int sum(List<int> intList, Func<int, int> f)
+        {
+            if (intList.Count == 0)
+                return 0;
+            else return f(intList[0]) + sum(intList.GetRange(1, intList.Count - 1), f);
+        }
+    }
+}
